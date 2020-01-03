03ebc3ac38df9bdc2b5731f9b996472f51873211
Ivam <ivam.developer@gmail.com>
2020-01-02 21:50:42 -0300

    Settings_03

--- application/models/ScheduleAuxModel.php
+++ application/models/ScheduleAuxModel.php
@@ -2,23 +2,35 @@
 
 	class ScheduleAuxModelModel extends CI_Model {
 		
-		public function getScheduleAuxModel() {
+		public function getScheduleAux() {
 			return $this->db->get('orkney10_konektron_cli.schedule_aux')->result();
 		}
 
-		public function getScheduleAuxModelId($id_schedule_aux) {
+		public function getScheduleAuxId($id_schedule_aux) {
 			return $this->db->get_where('orkney10_konektron_cli.schedule_aux', array('id_schedule_aux' => $id_schedule_aux))->row();
 		}
 
-		public function getScheduleAuxModelUsers($id_users) {
-			return $this->db->get_where('orkney10_konektron_cli.schedule_aux', array('id_users' => $id_users))->row();
+		public function getScheduleAuxIdSch($id_schedule) {
+			return $this->db->get_where('orkney10_konektron_cli.schedule_aux', array('id_schedule' => $id_schedule))->row();
 		}
 
-		public function insertScheduleAuxModel($scheduleAuxModel) {
+		public function getScheduleAuxUsers($id_users) {
+			return $this->db->get_where('orkney10_konektron_cli.schedule_aux', array('id_users' => $id_users))->row();
+		}		
+
+		public function getScheduleAuxProviders($id_providers) {
+			return $this->db->get_where('orkney10_konektron_cli.schedule_aux', array('id_providers' => $id_providers))->row();
+		}
+
+		public function getScheduleAuxService($id_service) {
+			return $this->db->get_where('orkney10_konektron_cli.schedule_aux', array('id_service' => $id_service))->row();
+		}
+
+		public function insertScheduleAux($scheduleAuxModel) {
 			$this->db->insert('orkney10_konektron_cli.schedule_aux', $scheduleAuxModel);
 		}
 
-		public function patchScheduleAuxModel($id_schedule_aux, $scheduleAuxModel) {
+		public function patchScheduleAux($id_schedule_aux, $scheduleAuxModel) {
 			$this->db->where('id_schedule_aux', $id_schedule_aux);
 			$this->db->update('orkney10_konektron_cli.schedule_aux', $scheduleAuxModel);
 
@@ -28,7 +40,7 @@
 			return NULL;
 		}
 
-		public function delScheduleAuxModel($id_schedule_aux) {
+		public function delScheduleAux($id_schedule_aux) {
 			$this->db->where('id_schedule_aux', $id_schedule_aux);
 			$this->db->delete('orkney10_konektron_cli.schedule_aux');
 		}
