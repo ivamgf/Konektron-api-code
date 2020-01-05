462e5d7817376b06f3453c0c5cee1a54b020c77b
Ivam <ivam.developer@gmail.com>
2020-01-05 20:19:02 -0300

    Sign_01

--- application/controllers/sign.php
+++ application/controllers/sign.php
@@ -3,11 +3,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Sign extends CI_Controller {
 	
-	public function signin()
+	public function signin($us_email, $us_password)
 	{
-		// $this->load->view('welcome_message');
-		echo "Signin";
-		exit;
+		$this->load->model('SignModel', 'signModel', true);
+		$signin = $this->signModel->signinUser($us_email, $us_password);
+		if(is_null($signin)) {
+			redirect(base_url(''));
+		} else {
+			$dataSignin = array(
+				"signin" => $signin
+			);
+	
+			$this->load->model('SignModel', 'signModel', true);
+			$auth = $this->signModel->getAuthUsers($id_auth);
+			$dataAuth = array(
+				"auth" => $auth
+			);
+
+			$session = array(
+				"id" => $signin -> id,
+				"user" => $signin -> user,
+				"email" => $signin -> email
+			);
+			$this -> session->set_userdata('usersSession', $session);
+		}
 	}
 
 	public function signinProviders()
--- application/models/SignModel.php
+++ application/models/SignModel.php
@@ -9,4 +9,12 @@
 		public function signupProviders($signupProviders) {
 			$this->db->insert('orkney10_konektron_cli.providers', $signupProviders);
 		}
+
+		public function signinUser($us_email, $us_password) {
+			return $this->db->get_where('orkney10_konektron_cli.users', array('us_email' => $us_email, 'us_password' => $us_password))->row();
+		}
+
+		public function getAuthUsers($id_auth) {
+			return $this->db->get_where('orkney10_konektron_cli.authorization', array('id_auth' => $id_auth))->row();
+		}
 	}
